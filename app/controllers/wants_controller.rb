f05d98bc5aabee565f300922f24476740e41e098
DAITADOKORO <bangasa0101@gmail.com>
2020-01-20 14:38:19 +0900

    start slideshow

--- app/controllers/wants_controller.rb
+++ app/controllers/wants_controller.rb
@@ -6,14 +6,15 @@ class WantsController < ApplicationController
 
   def show
     @user = User.find(params[:user_id])
-    @wants = @user.wants
+    @wants = @user.wants.page(params[:page]).per(10)
   end
 
   def tag_cloud
     @tags = Restaurant.tag_counts_on(:tags).order('count DESC')
     @restaurants = Restaurant.tagged_with(params[:tag_name])
     @hoge = Want.where(restaurant_id: @restaurants.ids)
-    @wants = @hoge.group(:restaurant_id).order("count(restaurant_id) desc")  end
+    @wants = @hoge.group(:restaurant_id).order("count(restaurant_id) desc")
+  end
 
   def create
       @restaurant = Restaurant.find(params[:restaurant_id])
